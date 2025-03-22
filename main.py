beaeee5e4335a2636a9363cd465f1e08e7337880
hizxle <torbionic@gmail.com>
2025-03-22 17:18:13 +0300

    Fixing token leak

--- main.py
+++ main.py
@@ -422,7 +422,7 @@ class TulipBot:
 
 
 if __name__ == "__main__":
-    TELEGRAM_TOKEN = "7836342933:AAEQUQXyCrNdWiz_KZnYhjOpYzTOsR9mBpU"
+    TELEGRAM_TOKEN = "INSERT_YOUR_TELEGRAM_TOKEN"
 
     if not TELEGRAM_TOKEN:
         logger.error("Не указан обязательный параметр TELEGRAM_TOKEN.")
